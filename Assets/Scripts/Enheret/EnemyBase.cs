2ab3aa0e7fe50c8318156fc7cc1cc1447df7d732
u-n79 <cooc.2010589@gmail.com>
2025-11-04 17:02:16 +0300

    Trying Thing

---
+++ Assets/Scripts/Enheret/EnemyBase.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    private int health;
+    private int damage;
+    private float speed;
+
+    void Update()
+    {
+        Debug.Log("");
+    }
+
+}
