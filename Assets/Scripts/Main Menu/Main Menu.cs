e756165a83848a0c9962c7ce89183c1f96290d2c
raneemhejazie <raneemhejazie@gmail.com>
2025-11-26 20:48:23 +0300

    scenes linked and done

--- Assets/Scripts/Main Menu/Main Menu.cs
+++ Assets/Scripts/Main Menu/Main Menu.cs
@@ -5,12 +5,18 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("NewMap");
+        // Load the Instruction Scene first
+        SceneManager.LoadScene("instructions");
     }
 
     public void QuitGame()
     {
         Debug.Log("Quitting game...");
         Application.Quit();
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
     }
 }
